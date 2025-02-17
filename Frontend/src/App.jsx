1fefadbd0f7420e366d0e0425cfa26f1c2c03b06
rahulsah009 <150267007+rahulsah009@users.noreply.github.com>
2025-02-17 11:58:29 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
+    const response = await axios.post("https://codesense-ai-wgdj.onrender.com", {
       code,
     });
 
